23811270dd3eff3bd27defc72cb6348813a7ab85
amateur-dev <dipesh.sukhani@gmail.com>
2019-05-17 18:38:37 +0800

    Custom User and our old fields

--- condo_project/condo_app/views.py
+++ condo_project/condo_app/views.py
@@ -18,15 +18,16 @@ def index(request):
     return render(request, 'landing_page/signup.html')
     
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = SignUpForm()
+    # if request.method == 'POST':
+    #     form = SignUpForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         raw_password = form.cleaned_data.get('password1')
+    #         user = authenticate(username=username, password=raw_password)
+    #         login(request, user)
+    #         return redirect('home')
+    # else:
+    #     form = SignUpForm()
+    form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
