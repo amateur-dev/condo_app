f80fc4c1b439c9a2299b644823135fdc59c69fd6
amateu-dev <dipesh.sukhani@gmail.com>
2019-05-22 01:07:32 +0800

    Django Signal related code change

--- condo_project/condo_app/models.py
+++ condo_project/condo_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.db.models.signals import post_save
 
 from .managers import CustomUserManager
 
@@ -15,7 +16,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    condo_name = models.CharField(max_length=64, blank=False, null=True)
+    condo_name = models.CharField(max_length=64, blank=True, null=True)
     unit_floor = models.PositiveSmallIntegerField(blank=False, null=True)
     unit_unit = models.PositiveSmallIntegerField(blank=False, null=True)
     has_access_to_facility = models.BooleanField(default=False, null=True)
@@ -32,22 +33,34 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         "Does the user have a specific permission?"
         return True
 
+    # def save[dipesh to work on this]
+
 
 class Condo(models.Model):
-    name = models.CharField(max_length=64)
-    address = models.CharField(max_length=64)
-    superuser = models.EmailField(max_length=64, unique=True, null=True)
+    name = models.CharField(max_length=64, blank=True, null=True)
+    address = models.CharField(max_length=64, blank=True, null=True)
+    condo_admin = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.id}: {self.name} located at {self.address}"
 
 
+def create_condo(sender, instance, **kwargs):
+    if kwargs['created']:
+        condo_name_var = Condo.objects.create(name=instance.condo_name)
+        # condo_name_var is for me to make further edits if I need to.
+
+
+post_save.connect(create_condo, sender=CustomUser)
+
+
 class Facility(models.Model):
-    name = models.CharField(max_length=64)
-    open_time = models.TimeField(null=True)
-    close_time = models.TimeField(null=True)
+    name = models.CharField(max_length=64, blank=True, null=True)
+    first_booking_time = models.TimeField(null=True)
+    last_booking_time_time = models.TimeField(null=True)
     condo = models.ForeignKey(
         Condo, on_delete=models.CASCADE, related_name="condo")
 
     def __str__(self):
-        return f"{self.origin} to {self.destination} in {self.duration} mins"
+        return f"{self.name} | {self.condo}"
